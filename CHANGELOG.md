96ed85f4d66b404daf0abf027ca726d61ac8e745
Long Nhat Nguyen <torn4dom4n@gmail.com>
2016-07-03 11:31:33 +0700

    Prepare 0.1.5 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,17 +1,24 @@
+# v0.1.5
+* Fix boot UEFI-IA32
+* Remove grub4dos
+* Change grub.cfg into main.cfg
+* Remove HBCD folder
+* Fix icon ubuntu and icon fedora
+* Creat grub2 i386-efi
+* Update i386-pc and x86_64-efi
+
 # v0.1.4
 * Fix boot WinPE
 
 # v0.1.3
-* Remove file
-* Creat wimxpe.wim and apps.wim
 * Creat HBCD folder boot grub4dos
 
 # v0.1.2
-* Remove some folders.
-Creat iso file.
+* Fix code
+* Creat iso file
 
 # v0.1.1
-* Fix boot linux iso.
+* Fix boot linux iso
 
 # v0.1.0
-* Init everything.
+* Init everything
--- boot/grub/i386-pc/modinfo.sh
+++ boot/grub/i386-pc/modinfo.sh
@@ -15,12 +15,12 @@ grub_bss_start_symbol="__bss_start"
 grub_end_symbol="end"
 
 # Build environment
-grub_target_cc='gcc-4.7'
-grub_target_cc_version='gcc-4.7 (Ubuntu/Linaro 4.7.3-12ubuntu1) 4.7.3'
-grub_target_cflags=' -Os -Wall -W -Wshadow -Wpointer-arith -Wundef -Wchar-subscripts -Wcomment -Wdeprecated-declarations -Wdisabled-optimization -Wdiv-by-zero -Wfloat-equal -Wformat-extra-args -Wformat-security -Wformat-y2k -Wimplicit -Wimplicit-function-declaration -Wimplicit-int -Wmain -Wmissing-braces -Wmissing-format-attribute -Wmultichar -Wparentheses -Wreturn-type -Wsequence-point -Wshadow -Wsign-compare -Wswitch -Wtrigraphs -Wunknown-pragmas -Wunused -Wunused-function -Wunused-label -Wunused-parameter -Wunused-value  -Wunused-variable -Wwrite-strings -Wnested-externs -Wstrict-prototypes -g -Wredundant-decls -Wmissing-prototypes -Wmissing-declarations  -Wextra -Wattributes -Wendif-labels -Winit-self -Wint-to-pointer-cast -Winvalid-pch -Wmissing-field-initializers -Wnonnull -Woverflow -Wvla -Wpointer-to-int-cast -Wstrict-aliasing -Wvariadic-macros -Wvolatile-register-var -Wpointer-sign -Wmissing-prototypes -Wmissing-declarations -Wformat=2 -march=i386 -m32 -mrtd -mregparm=3 -falign-jumps=1 -falign-loops=1 -falign-functions=1 -freg-struct-return -mno-mmx -mno-sse -mno-sse2 -mno-3dnow -fno-dwarf2-cfi-asm -fno-asynchronous-unwind-tables -Qn -fno-stack-protector -Wtrampolines -Werror'
-grub_target_cppflags='-Wno-unused-but-set-variable -Wall -W -I$(top_srcdir)/include -I$(top_builddir)/include  -DGRUB_MACHINE_PCBIOS=1 -DGRUB_MACHINE=I386_PC -m32 -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/4.7/include'
+grub_target_cc='gcc-5'
+grub_target_cc_version='gcc-5 (Ubuntu 5.3.1-20ubuntu1) 5.3.1 20160519'
+grub_target_cflags=' -Os -Wall -W -Wshadow -Wpointer-arith -Wundef -Wchar-subscripts -Wcomment -Wdeprecated-declarations -Wdisabled-optimization -Wdiv-by-zero -Wfloat-equal -Wformat-extra-args -Wformat-security -Wformat-y2k -Wimplicit -Wimplicit-function-declaration -Wimplicit-int -Wmain -Wmissing-braces -Wmissing-format-attribute -Wmultichar -Wparentheses -Wreturn-type -Wsequence-point -Wshadow -Wsign-compare -Wswitch -Wtrigraphs -Wunknown-pragmas -Wunused -Wunused-function -Wunused-label -Wunused-parameter -Wunused-value  -Wunused-variable -Wwrite-strings -Wnested-externs -Wstrict-prototypes -g -Wredundant-decls -Wmissing-prototypes -Wmissing-declarations  -Wextra -Wattributes -Wendif-labels -Winit-self -Wint-to-pointer-cast -Winvalid-pch -Wmissing-field-initializers -Wnonnull -Woverflow -Wvla -Wpointer-to-int-cast -Wstrict-aliasing -Wvariadic-macros -Wvolatile-register-var -Wpointer-sign -Wmissing-prototypes -Wmissing-declarations -Wformat=2 -march=i386 -m32 -mrtd -mregparm=3 -falign-jumps=1 -falign-loops=1 -falign-functions=1 -freg-struct-return -mno-mmx -mno-sse -mno-sse2 -mno-3dnow -fno-dwarf2-cfi-asm -fno-asynchronous-unwind-tables -Qn -fno-PIE -fno-stack-protector -Wtrampolines -Werror'
+grub_target_cppflags='-Wno-unused-but-set-variable -Wall -W -I$(top_srcdir)/include -I$(top_builddir)/include  -DGRUB_MACHINE_PCBIOS=1 -DGRUB_MACHINE=I386_PC -m32 -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/5/include'
 grub_target_ccasflags=' -g -m32'
-grub_target_ldflags=' -m32 -Wl,-melf_i386 -Wl,--build-id=none'
+grub_target_ldflags='-no-pie -m32 -Wl,-melf_i386 -Wl,--build-id=none'
 grub_target_strip='strip'
 grub_target_nm='nm'
 grub_target_ranlib='ranlib'
@@ -30,7 +30,7 @@ grub_target_obj2elf=''
 # Version
 grub_version="2.02~beta2"
 grub_package="grub"
-grub_package_string="GRUB 2.02~beta2-9ubuntu1.2"
-grub_package_version="2.02~beta2-9ubuntu1.2"
+grub_package_string="GRUB 2.02~beta2-36ubuntu8"
+grub_package_version="2.02~beta2-36ubuntu8"
 grub_package_name="GRUB"
 grub_package_bugreport="bug-grub@gnu.org"
